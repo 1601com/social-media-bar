fa92d98f9056a9647ea7f1f14a81836f1c8db46a
Frank Ficzak <ff@1601.com>
2020-06-24 15:38:44 +0200

    fixed language files for social-media-bar

--- agentur1601com/social-media-bar/src/Resources/contao/languages/de/tl_smbar_configuration.php
+++ agentur1601com/social-media-bar/src/Resources/contao/languages/de/tl_smbar_configuration.php
@@ -4,8 +4,8 @@
 $GLOBALS['TL_LANG']['tl_smbar_configuration']['new'] = ['Neues Social Media Element'];
 
 $GLOBALS['TL_LANG']['tl_smbar_configuration']['title_legend'] = 'Social Media Bar Element';
-$GLOBALS['TL_LANG']['tl_smbar_configuration']['smtypeselect'] = ['Soziales Medium', 'Hier kann ein gewünschtes socziales Medium ausgewählt werden'];
-$GLOBALS['TL_LANG']['tl_smbar_configuration']['smurl'] = ['Social Media Link', 'Hier kann der Social Media Link zur entsprechenden Social Media Seite hinterlegt werden.'];
+$GLOBALS['TL_LANG']['tl_smbar_configuration']['smType'] = ['Soziales Medium', 'Hier kann ein gewünschtes socziales Medium ausgewählt werden'];
+$GLOBALS['TL_LANG']['tl_smbar_configuration']['smURL'] = ['Social Media Link', 'Hier kann der Social Media Link zur entsprechenden Social Media Seite hinterlegt werden.'];
 
 $GLOBALS['TL_LANG']['tl_smbar_configuration']['icon_legend'] = 'Social Media Bar Icon';
 $GLOBALS['TL_LANG']['tl_smbar_configuration']['smcustomicon'] = ['Custom Icon', 'Hier kann ein eigenes Social Media icon ausgewählt werden. Standardmäßig wird für jedes vordefinierte Soziale Medium ein weißes Icon ausgegeben.'];
