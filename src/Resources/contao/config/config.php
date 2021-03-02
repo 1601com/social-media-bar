6ee0eec89dfeb6dc3b61957c2e8013d6590fdbb4
Valentin Mayer <vm@1601.com>
2021-03-02 17:55:26 +0100

    remove unused code

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,10 +1,5 @@
 <?php
 
-if (TL_MODE == "FE") {
-    //$GLOBALS['TL_CSS'][] = 'bundles/socialmediabar/style.css';
-    //$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/socialmediabar/popup.js';
-}
-
 $GLOBALS['BE_MOD']['agentur1601com']['social_media_bar'] = [
     'tables' => ['tl_smbar', 'tl_smbar_configuration'],
 ];
